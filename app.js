3e1b69a0723730f22626895ce055b6f9da6f14a1
srijit-ops <srijitasengupta23@gmail.com>
2022-04-11 18:24:00 +0530

    files added

---
+++ app.js
@@ -0,0 +1,98 @@
+//targeting
+const input=document.querySelectorAll(".task_input")
+const add_button=document.querySelectorAll(".task_add")
+const task_list=document.querySelectorAll(".task_container")
+const filterOption=document.querySelectorAll(".selection")
+
+//click event adding
+
+add_button[0].addEventListener("click",addData)
+task_list[0].addEventListener("click",doneDel)
+filterOption[0].addEventListener("click",filter_todo)
+
+//input adding function
+function addData(click){
+    //prevent form submitting
+    click.preventDefault()
+
+    //create list container which will contain li and 2 buttons.
+    const li_container=document.createElement("div")
+    
+    li_container.classList.add("liDiv")
+
+    //create li
+    const listItem=document.createElement("li")
+    listItem.classList.add("data")
+    listItem.innerText=input[0].value
+    li_container.appendChild(listItem) //add the li in list container div
+
+    //create done button
+    const done=document.createElement("button")
+    done.classList.add("checkButton")
+    done.innerText="Done"
+    li_container.appendChild(done) //add the done button in list container div
+
+    //create delete button
+    const del=document.createElement("button")
+    del.classList.add("delButton")
+    del.innerText="Delete"
+    li_container.appendChild(del) //add the delete button in list container div
+
+    task_list[0].appendChild(li_container)//add the whole list container div in the ul div
+
+    
+
+    input[0].value="" //To clear the input box after every time we add the input
+}
+
+//complete and uncomplete buttons
+
+function doneDel(event){
+    const targetbyClick=event.target
+
+    //delete item
+    if(targetbyClick.classList[0]==="delButton"){
+        const del_Div=targetbyClick.parentElement
+        del_Div.remove()
+    }else if(targetbyClick.classList[0]==="checkButton"){ //completed item
+        const checked_div=targetbyClick.parentElement
+        //console.log(checked_div)
+        checked_div.classList.toggle("task_done")
+
+        targetbyClick.disabled=true //disable the done button after clicking the done button.
+    }
+}
+
+
+//filter todo list
+function filter_todo(event){
+  const targetLiDivs=document.getElementsByClassName("liDiv")
+
+  switch(event.target.value){
+    case "all":
+      for(let i=0;i<targetLiDivs.length;i++){
+        targetLiDivs[i].style.display="flex"
+      }
+      break
+
+    case "completed":
+      for(let i=0;i<targetLiDivs.length;i++){
+        if(targetLiDivs[i].classList.contains("task_done")){
+          targetLiDivs[i].style.display="flex"
+        }else{
+          targetLiDivs[i].style.display="none"
+        }
+      }
+      break
+
+    case "uncompleted":
+      for(let i=0;i<targetLiDivs.length;i++){
+        if(targetLiDivs[i].classList.contains("task_done")){
+          targetLiDivs[i].style.display="none"
+        }else{
+          targetLiDivs[i].style.display="flex"
+        }
+      }
+      break
+  }
+}
